f104838d9c20b72ab77c43cd0935973926470091
jianxiong <jianxiong@canaan-creative.com>
2016-09-09 20:27:30 +0800

    modify path

--- install_desktop_script.sh
+++ install_desktop_script.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-cd ~/
+cd /home/
 git clone https://github.com/JianXiongZ/Burn_tool_script.git
 cd Burn_tool_script/
-    cp BurnAvalon7_mm.desktop BurnAvalon7_pmu.desktop TestAvalon7_pmu.desktop 说明.txt ~/Desktop/
+    cp BurnAvalon7_mm.desktop BurnAvalon7_pmu.desktop TestAvalon7_pmu.desktop README.txt ~/Desktop/
