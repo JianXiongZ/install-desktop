d40223e22c5dd3f1891b834e37f866612f6a8adc
jianxiong <jianxiong@canaan-creative.com>
2016-09-09 18:11:29 +0800

    install desktop

---
+++ install_desktop_script.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+cd ~/
+git clone https://github.com/JianXiongZ/Burn_tool_script.git
+cd Burn_tool_script/
+    cp BurnAvalon7_mm.desktop BurnAvalon7_pmu.desktop TestAvalon7_pmu.desktop 说明.txt ~/Desktop/
