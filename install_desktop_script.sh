0826050681c4cd4c7768b6b3c366dc100f3dfd9d
jianxiong <jianxiong@canaan-creative.com>
2016-12-26 12:28:10 +0800

    add 741

--- install_desktop_script.sh
+++ install_desktop_script.sh
@@ -2,4 +2,4 @@
 cd ~/
 git clone https://github.com/JianXiongZ/Burn_tool_script.git
 cd Burn_tool_script/
-    cp BurnAvalon7_mm.desktop BurnAvalon7_mm2.desktop BurnAvalon7_pmu.desktop TestAvalon7_pmu.desktop README.txt ~/Desktop/
+    cp BurnAvalon7_721mm.desktop BurnAvalon7_741mm.desktop BurnAvalon7_721pmu.desktop BurnAvalon7_741pmu.desktop TestAvalon7_pmu.desktop README.txt ~/Desktop/
